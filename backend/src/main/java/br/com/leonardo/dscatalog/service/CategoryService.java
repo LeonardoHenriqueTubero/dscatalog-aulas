2e4cb30fac8d16087c8a78f931479c436e76defe
Leoanrdo <leonardohenriquetubero@gmail.com>
2025-09-05 11:50:24 -0300

    Database seeding

--- backend/src/main/java/br/com/leonardo/dscatalog/service/CategoryService.java
+++ backend/src/main/java/br/com/leonardo/dscatalog/service/CategoryService.java
@@ -4,6 +4,7 @@ import br.com.leonardo.dscatalog.entities.Category;
 import br.com.leonardo.dscatalog.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -17,6 +18,7 @@ public class CategoryService {
         this.repository = repository;
     }
 
+    @Transactional(readOnly = true)
     public List<Category> findAll() {
         return repository.findAll();
     }
